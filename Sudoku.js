f15456bdd399ee23b3d34e862d689b31b76f057a
Thomass Muir <thomassmuir@Thomasss-MacBook-Pro.local>
2020-04-12 14:45:41 -0400

    Final Project

--- Sudoku.js
+++ Sudoku.js
@@ -1,6 +1,23 @@
-/**
- * Created by thomassmuir on 2019-02-12.
+/*
+
+ _______ _______ _______ _______ _______ _______
+|\     /|\     /|\     /|\     /|\     /|\     /|
+| +---+ | +---+ | +---+ | +---+ | +---+ | +---+ |
+| |   | | |   | | |   | | |   | | |   | | |   | |
+| |S  | | |u  | | |d  | | |o  | | |k  | | |u  | |
+| +---+ | +---+ | +---+ | +---+ | +---+ | +---+ |
+|/_____\|/_____\|/_____\|/_____\|/_____\|/_____\|
+
+
+By Thomass Muir ©2020, Sudoku v1.0 - April 2020 
+    Student ID: t2muir / 20837270
+    Contact: t2muir@uwaterloo.ca
+    Website: sudoku.thomassmuir.com
+
+DOCUMENTATION:
+    External Documentation – "readme.pdf"
  */
+
 
 // constant
 let play;
@@ -12,17 +29,40 @@ let about; //Image variables for Canvas
 // state variables
 let selectedScreen = 0;
 let chosen;
+let chosenSquare;
 let button = "";
 let loadingFile = false;
 let isSolved = false;
 let displayGame = false;
 let generated = false;
+let chosenBorder = {};
 
-// Aarays for board generation
+// variables for saving
+let save = false;
+let gameStorageJSON = {};
+
+// arrays for board generation
 let board = boardArray(); // board array that renders out on the console
 let initial = boardArray(); // initial array generation for later comparison and auto-solve
 let solved = boardArray(); // solved array of current board
 let loaded = boardArray(); // loaded array of user-loaded .sudoku file
+
+// colour variables for highlighting
+let highlightCol = '#418af0'; // lavender
+let eraseCol = '#f2f0fa'; // light lavender
+let col1 = '#FFC2A8'; // orange
+let col2 = '#F1A49F'; // salmon
+let col3 = '#DB889D'; // pink
+let col4 = '#BC729E'; // mauve
+let col5 = '#9262A0'; // purple
+let col6 = '#58569D'; // indigo
+let col7 = '#4451C6'; // blue
+let col8 = '#009EA2'; // teal
+let col9 = '#00A881'; // green
+
+let saveInput;
+let saveButton;
+let loadButton;
 
 function preload() {
   // splash = loadImage('data/sudokusplash.png');
@@ -32,16 +72,16 @@ function preload() {
   play = loadImage('data/sudokuboard.png');
 }
 
-// Main class
+// main class
 function setup() {
 
-  // Calls upon generateBoard() class to generate a working Sudoku board
-  // Calls upon generatePuzzle() class to create a randomized solvable puzzle
+  // calls upon generateBoard() class to generate a working Sudoku board
+  // calls upon generatePuzzle() class to create a randomized solvable puzzle
   generateBoard();
   generatePuzzle();
   generated = true;
 
-  // Sets initial int array to values of newly generated board array
+  // sets initial int array to values of newly generated board array
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++) {
       initial[x][y] = int(board[x][y]);
@@ -51,11 +91,27 @@ function setup() {
   // sets text properties
   textFont('Tahoma');
   textSize(30);
-
   noFill();
 
   // Creates canvas
   createCanvas(640, 480);
+
+  // Formats "Load" and "Save" buttons for the game
+  createP(); // spacing
+  createP(); 
+
+  // Creates Wrapper Element for styling (st) purposes
+  let wrapper = createElement('div');
+  let btn = createButton("Load Puzzle"); // DOM button element
+  btn.id("puzzle-btn"); // st
+  wrapper.id("load-puzzle"); //st
+  loadButton = createFileInput(loadData); // Opens Dialogue Box
+  btn.parent('load-puzzle'); //st
+  loadButton.parent('load-puzzle'); //st
+  createP();
+  saveButton = createButton('Save Puzzle');
+  saveButton.mousePressed(saveData); 
+
 }
 
 function draw() {
@@ -64,37 +120,42 @@ function draw() {
 
   // splash screen buttons (highlight pink)
   if (selectedScreen === 0) {
-    highlightButtons(mouseX, mouseY, 440, 246, 100, 39, '#ffb0c5'); // play button
-    highlightButtons(mouseX, mouseY, 460, 286, 54, 17, '#ffb0c5'); // rules button
-    highlightButtons(mouseX, mouseY, 460, 304, 53, 16, '#ffb0c5'); // about button
+    highlightButtons(mouseX, mouseY, 440, 246, 100, 39, highlightCol); // play button
+    highlightButtons(mouseX, mouseY, 460, 286, 54, 17, highlightCol); // rules button
+    highlightButtons(mouseX, mouseY, 460, 304, 53, 16, highlightCol); // about button
+  }
+
+  // if number is chosen, highlights the number with it's designated colour
+  // for as long as it is chosen
+  if (selectedScreen === 1 && chosen !== undefined) {
+    highlightButtons(-1, -1, chosenBorder.x, chosenBorder.y, chosenBorder.w, chosenBorder.h, chosenBorder.col);
   }
 
   // number buttons (hightlight colour according to number)
   if (selectedScreen === 1) {
-    highlightButtons(mouseX, mouseY, 433, 65, 46, 47, '#ffb0c5');  // number 1, pink highlight
-    highlightButtons(mouseX, mouseY, 489, 65, 46, 47, '#8edd8b');  // number 2, green highlight
-    highlightButtons(mouseX, mouseY, 545, 65, 46, 47, '#9ff2f9');  // number 3, blue highlight
-    highlightButtons(mouseX, mouseY, 433, 122, 46, 47, '#bb94ff'); // number 4, purple highlight
-    highlightButtons(mouseX, mouseY, 489, 122, 46, 47, '#fffff5'); // number 5, beige highlight
-    highlightButtons(mouseX, mouseY, 545, 122, 46, 47, '#ffb850'); // number 6, orange highlight
-    highlightButtons(mouseX, mouseY, 433, 177, 46, 47, '#97ffc1'); // number 7, mint highlight
-    highlightButtons(mouseX, mouseY, 489, 177, 46, 47, '#ffed7a'); // number 8, yellow highlight
-    highlightButtons(mouseX, mouseY, 545, 177, 46, 47, '#fd908e'); // number 9, yellow highlight
+    highlightButtons(mouseX, mouseY, 433, 65, 46, 47, col1);  // number 1, pink highlight
+    highlightButtons(mouseX, mouseY, 489, 65, 46, 47, col2);  // number 2, green highlight
+    highlightButtons(mouseX, mouseY, 545, 65, 46, 47, col3);  // number 3, blue highlight
+    highlightButtons(mouseX, mouseY, 433, 122, 46, 47, col4); // number 4, purple highlight
+    highlightButtons(mouseX, mouseY, 489, 122, 46, 47, col5); // number 5, beige highlight
+    highlightButtons(mouseX, mouseY, 545, 122, 46, 47, col6); // number 6, orange highlight
+    highlightButtons(mouseX, mouseY, 433, 177, 46, 47, col7); // number 7, mint highlight
+    highlightButtons(mouseX, mouseY, 489, 177, 46, 47, col8); // number 8, yellow highlight
+    highlightButtons(mouseX, mouseY, 545, 177, 46, 47, col9); // number 9, yellow highlight
   }
 
   // main screen buttons (hightlight pink)
   if (selectedScreen === 1) {
-    highlightButtons(mouseX, mouseY, 291, 22, 128, 30, '#ffb0c5'); //auto solve button
-    highlightButtons(mouseX, mouseY, 556, 6, 77, 29, '#ffb0c5'); // erase button
-    highlightButtons(mouseX, mouseY, 521, 4, 30, 34, '#ffb0c5'); // home button
-    highlightButtons(mouseX, mouseY, 437, 256, 154, 38, '#ffb0c5'); //load puzzle button
-    highlightButtons(mouseX, mouseY, 437, 305, 154, 38, '#ffb0c5'); //solve puzzle button
-    highlightButtons(mouseX, mouseY, 437, 355, 154, 38, '#ffb0c5'); //new puzzle button
+    highlightButtons(mouseX, mouseY, 437, 305, 154, 38, highlightCol); //auto solve button
+    highlightButtons(mouseX, mouseY, 556, 6, 77, 29, eraseCol); // erase button
+    highlightButtons(mouseX, mouseY, 521, 4, 30, 34, highlightCol); // home button
+    highlightButtons(mouseX, mouseY, 437, 256, 154, 38, highlightCol); // new puzzle button
+    highlightButtons(mouseX, mouseY, 437, 355, 154, 38, highlightCol); // back to home button 2 (more visible)
   }
 
   // rules / about screen (highlight pink)
   if (selectedScreen === 2 || selectedScreen === 3) {
-    highlightButtons(mouseX, mouseY, 452, 41, 116, 39, '#ffb0c5'); // return to splash button
+    highlightButtons(mouseX, mouseY, 452, 41, 116, 39, highlightCol); // return to splash button
   }
 
   //If statement to print congratulations message if sudoku puzzle is solved
@@ -140,23 +201,25 @@ function mouseReleased() {
   }
 
   // main screen buttons
-  if (selectedScreen === 1) {
+  else if (selectedScreen === 1) {
     // home button
-    if (rectHitTest(mouseX, mouseY, 521, 4, 30, 34)) {
-      alert("Warning! A new puzzle will be generated when you return to the Home menu.");
-      gameReset();
-      displayGame = false;
+    if (rectHitTest(mouseX, mouseY, 521, 4, 30, 34) || rectHitTest(mouseX, mouseY, 437, 355, 154, 38)) {
+      if (confirm("Warning! A new puzzle will be generated when you return to the Home menu.")) {
+        gameReset();
+        displayGame = false;
+      }
     }
     // erase button
     if (rectHitTest(mouseX, mouseY, 556, 6, 77, 29)) {
       chosen = 0;
+      chosenRecordCreator(556, 6, 77, 29, eraseCol);
     }
     // new puzzle button
-    if (rectHitTest(mouseX, mouseY, 438, 350, 151, 36)) {
+    if (rectHitTest(mouseX, mouseY, 437, 256, 154, 38)) {
       gameReset();
     }
     // auto solve button
-    if (rectHitTest(mouseX, mouseY, 291, 22, 128, 30)) {
+    if (rectHitTest(mouseX, mouseY, 437, 305, 154, 38)) {
       revealPuzzle();
     }
 
@@ -165,37 +228,45 @@ function mouseReleased() {
     // numbers clicked become the chosen number to place
     if (rectHitTest(mouseX, mouseY, 433, 65, 46, 47)) {
       chosen = 1;
+      chosenRecordCreator(433, 65, 46, 47, col1);
     } else if (rectHitTest(mouseX, mouseY, 489, 65, 46, 47)) {
       chosen = 2;
+      chosenRecordCreator(489, 65, 46, 47, col2);
     } else if (rectHitTest(mouseX, mouseY, 545, 65, 46, 47)) {
       chosen = 3;
+      chosenRecordCreator(545, 65, 46, 47, col3);
     } else if (rectHitTest(mouseX, mouseY, 433, 122, 46, 47)) {
       chosen = 4;
+      chosenRecordCreator(433, 122, 46, 47, col4);
     } else if (rectHitTest(mouseX, mouseY, 489, 122, 46, 47)) {
       chosen = 5;
+      chosenRecordCreator(489, 122, 46, 47, col5);
     } else if (rectHitTest(mouseX, mouseY, 545, 122, 46, 47)) {
       chosen = 6;
+      chosenRecordCreator(545, 122, 46, 47, col6);
     } else if (rectHitTest(mouseX, mouseY, 433, 177, 46, 47)) {
       chosen = 7;
+      chosenRecordCreator(433, 177, 46, 47, col7);
     } else if (rectHitTest(mouseX, mouseY, 489, 177, 46, 47)) {
       chosen = 8;
+      chosenRecordCreator(489, 177, 46, 47, col8);
     } else if (rectHitTest(mouseX, mouseY, 545, 177, 46, 47)) {
       chosen = 9;
+      chosenRecordCreator(545, 177, 46, 47, col9);
     }
 
-    //Set chosenSquare variable to whatever placeNum function returns
+    // set chosenSquare variable to whatever placeNum function returns
     chosenSquare = selectedSquare(mouseX, mouseY);
-    //If statement to write what editable square becomes a chosen variable when clicked
-    //And then print it
-    if (chosenSquare !== null && editable(chosenSquare) && chosen) {
 
+    // if statement to write what editable square becomes a chosen variable when clicked
+    // and then print it
+    if (chosenSquare !== null && editable(chosenSquare) && (chosen || chosen === 0)) {
       board[chosenSquare.x][chosenSquare.y] = chosen;
     }
-
-
   }
+
   // rule / about screen
-  if (selectedScreen === 2 || selectedScreen === 3) {
+  else if (selectedScreen === 2 || selectedScreen === 3) {
     // return to splash button
     if (rectHitTest(mouseX, mouseY, 452, 41, 116, 39)) {
       selectedScreen = 0;
@@ -203,12 +274,30 @@ function mouseReleased() {
   }
 }
 
-// function that highlights button when mouse hovers over it
+// changes the values of the records for the chosenBorder container
+// is used to highlight chosen number or eraser
+function chosenRecordCreator(x, y, w, h, col) {
+  chosenBorder.x = x;
+  chosenBorder.y = y;
+  chosenBorder.w = w;
+  chosenBorder.h = h;
+  chosenBorder.col = col;
+}
+
+// function that highlights button when mouse hovers over it or is clicked
 // uses rectHitTest function to determine if mouse is hovering over button
 // adds a thicker stroke button around hover area
 // location and colour are determined by parameters
 function highlightButtons(px, py, x, y, w, h, col) {
   noFill();
+  // if -1 is entered for px and py, do not treat as a mouse over
+  if (px === -1 && py === -1) {
+    strokeWeight(4)
+    strokeWeight(4);
+    stroke(col);
+    rect(x, y, w, h, 5);
+  }
+
   if (rectHitTest(px, py, x, y, w, h, col)) {
     strokeWeight(4);
     stroke(col);
@@ -255,8 +344,8 @@ function copyBoard(arr) {
 }
 
 // compareBoard boolean function that compares both of the Integer arrays passed as the arguments
-// If the value at each coordinate is equal to one another, the function returns false
-// Otherwise, it returns true
+// if the value at each coordinate is equal to one another, the function returns false
+// otherwise, it returns true
 function compareBoard(arr1, arr2) {
   for (let y = 0; y < 9; y++) {
     for (let x = 0; x < 9; x++)
@@ -267,13 +356,13 @@ function compareBoard(arr1, arr2) {
   return true;
 }
 
-// Resets Puzzle
-// Alerts the player that there will be a new board set up
-// Might take a few moments to generate puzzle.
+// resets Puzzle
+// alerts the player that there will be a new board set up
+// might take a few moments to generate puzzle.
 function gameReset() {
   isSolved = true; // resets puzzle
   selectedScreen = 0;
-  alert("We are now generating a new sudoku board for you. This might take a few moments ^^");
+  alert("We are now generating a new sudoku board for you. Afterwards, you will be redirected to the homescreen. This might take a few moments ^^");
   generateBoard();
   solved = copyBoard(board);
   generatePuzzle();
@@ -281,7 +370,7 @@ function gameReset() {
   isSolved = false;
 }
 
-//Reveals Puzzle
+// reveals Puzzle
 function revealPuzzle() {
   try {
     board = copyBoard(solved);
@@ -298,6 +387,10 @@ function revealPuzzle() {
 // if the user fills in the wrong number, the numbers turn red
 function drawGame() {
   if (selectedScreen === 1) {
+    if (chosen === 0) {
+      noStroke();
+      noFill();
+    }
     for (let y = 0; y < 9; y++) {
       for (let x = 0; x < 9; x++) {
         if (board[x][y] != 0) {
@@ -315,18 +408,18 @@ function drawGame() {
   }
 }
 
-//placeNum function that takes x,y, coordinate in as argument
-//And returns a CoordinatePair object
+// placeNum function that takes x,y, coordinate in as argument
+// and returns a CoordinatePair object
 function selectedSquare(x, y) {
 
-  //Creates new CoordinatePair object
+  // creates new CoordinatePair object
   let square = createVector(0, 0);
 
-  //If mouse clicked is within this area (area of Sudoku board)
-  //Then it indicates a square that can be clicked
+  // if mouse clicked is within this area (area of Sudoku board)
+  // then it indicates a square that can be clicked
   if (x > 30 && x < 418 && y > 67 && y < 451) {
 
-    //For loop to determine which column the square is in
+    // for loop to determine which column the square is in
     for (let i = 0; i < 9; i++) {
       if (x < ((i + 1) * 43 + 30)) {
         square.x = i;
@@ -334,7 +427,7 @@ function selectedSquare(x, y) {
       }
     }
 
-    //for loop to determine which row the square is in
+    // for loop to determine which row the square is in
     for (let i = 0; i < 9; i++) {
       if (y < ((i + 1) * 43 + 65)) {
         square.y = i;
@@ -342,21 +435,19 @@ function selectedSquare(x, y) {
       }
     }
 
-    //Returns CoordinatePair of square coordinate
+    // returns CoordinatePair of square coordinate
     return square;
   } else return null;//otherwise returns null
 }
 
-//Boolean editable function that takes in chosenSquare CoordinatePair object
-//as argument
+// boolean editable function that takes in chosenSquare CoordinatePair objec as argument
 function editable(chosenSquare) {
 
   //returns true if the initial board creation coordinate was empty (equals zero)
   return initial[chosenSquare.x][chosenSquare.y] === 0;
 }
 
-//Boolean isSame function that takes in chosenSquare CoordinatePair object
-//as argument
+// boolean isSame function that takes in chosenSquare CoordinatePair object as argument
 function isNotSame(chosenSquare) {
 
   //returns false if there is no square chosen
@@ -369,52 +460,38 @@ function isNotSame(chosenSquare) {
   return !(board[chosenSquare.x][chosenSquare.y] === solved[chosenSquare.x][chosenSquare.y]);
 }
 
+// saves the current board states to a JSON file
+function saveData() {
+  if (selectedScreen === 1) {
+    gameStorageJSON.boardState = board;
+    gameStorageJSON.initialState = initial;
+    gameStorageJSON.solvedState = solved;
+    saveJSON(gameStorageJSON, "current board");
+  } else {
+    alert("Sorry, there is no puzzle to save on this screen.");
+  }
 
+}
 
+// loads the boards states and calls the loadBoard function
+function loadData(file) {
+  loadJSON(file.data, loadBoard);
+}
 
-//////////////////////////////////////
-
-// //If statement to determine how the numbers appear when pressed
-// //If the number placed is wrong, it will appear red when placed
-// //Otherwise, it will appear black
-// if (!loadingFile)
-//   for (int y = 0; y < 9; y++) {
-//   for (int x = 0; x < 9; x++) {
-//     if (MouseClick.isSame(MouseClick.chosenSquare) && MouseClick.chosenSquare.x == x && MouseClick.chosenSquare.y == y) {
-//       g.setColor(Color.red); //appear red if wrong
-//     } else
-//       g.setColor(Color.black); //otherwise, appear
-
-//     //if statement to determine if the square that player has chosen is able to be written in
-//     //Prevents players from changing the board to their liking
-//     if (BoardCreator.board[x][y] != 0)
-//       g.drawString(BoardCreator.board[x][y] + "", x * 43 + 45, y * 43 + 93);
-//   }
-// }
-
-// //If Load Puzzle button is pressed, loadFile method is selected (Filter Class)
-// //Users will be able to selec
-// if (button.equals("Load Puzzle")) {
-//   SwingUtilities.invokeLater(new Runnable() { //waits until correct time for everything to load (prevents errors)
-//           @Override
-//   public void run() {
-//     Filter.loadFile();
-//     solved = false;
-//   }
-// }
-
-// //If Save Puzzle button is pressed, saveFile (Filter Class)
-// //Users will be able to name and save their current board state
-// if (button.equals("Save Puzzle")) {
-//   JOptionPane.showMessageDialog(Window.frame, "Warning! Do not save your current game state with mistakes, " +
-//     "they will not be able to be undone.");
-//   SwingUtilities.invokeLater(new Runnable() {  //waits until correct time for everything to load (prevents errors)
-//           @Override
-//   public void run() {
-//     Filter.saveFile();
-//   }
-// }
-
+// changes the board states to match the loaded file
+function loadBoard(json) {
+  try {
+    board = json["boardState"];
+    initial = json["initialState"];
+    solved = json["solvedState"];
+    if (!board || !initial || !solved) {
+      throw Exception();
+    }
+  } catch (e) {
+    alert("Puzzle file corrupt!");
+    location.reload(); // reload the window if game state is corrupted
+  }
+}
 
 //function that creates new board for solving by calling upon other function
 function generateBoard() {
@@ -543,7 +620,6 @@ function findLowestCandidates() {
   // return a random square from the final candidates array
   return random(finalCandidates);
 }
-//end of findLowestCandidates function
 
 // countCandidates function that returns array of ints
 // each additional data point in array is the number of candidates for each each square
@@ -562,7 +638,7 @@ function countCandidates(x, y) {
     return numbers;
   }
 
-  // for loop that determines what numbers the empty square can't be in its specific row
+  // for loop that determines what numbers the empty square can't be (in its specific row)
   // each number present in that row is removed from the array.
   for (let row = 0; row < 9; row++) {
     if (numbers.includes(board[row][y])) {
